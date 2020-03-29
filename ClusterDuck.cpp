0b6ce652c9b71b1f58d2174520285a8eb57e695c
Salzi <git@0111.ch>
2020-03-29 18:55:44 +0200

    add doxygen comments
    Signed-off-by: Salzi <git@0111.ch>

--- ClusterDuck.cpp
+++ ClusterDuck.cpp
@@ -7,23 +7,42 @@ AsyncWebServer webServer(80);
 
 auto tymer = timer_create_default();
 
-String ClusterDuck::_deviceId = "";
+String ClusterDuck::_deviceId = ""; ///< holds the device id
 
+
+/**
+ * @brief Constructor
+ */
 ClusterDuck::ClusterDuck() {
 
 
 }
 
+/**
+ * @brief Set device ID
+ * 
+ * @param deviceId used device ID. Do not leave deviceId _null_ or as an empty string.
+ */
 void ClusterDuck::setDeviceId(String deviceId) {
   _deviceId = deviceId;
 
 }
 
+/**
+ * @brief Initialize baude rate for serial. Use in `setup()`.
+ * 
+ * @param baudRate desired baude rate
+ */
 void ClusterDuck::begin(int baudRate) {
   Serial.begin(baudRate);
   Serial.println("Serial start");
 }
 
+/**
+ * @brief Initializes LED screen on Heltec LoRa ESP32 and configures it to show status, device ID, and the device type. Use in `setup()`.
+ * 
+ * @param deviceType TODO: valid values for deviceType
+ */
 void ClusterDuck::setupDisplay(String deviceType)  {
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -47,7 +66,15 @@ void ClusterDuck::setupDisplay(String deviceType)  {
   u8x8.print(duckMac(false));
 }
 
-// Initial LoRa settings
+/**
+ * @brief Initial LoRa settings
+ * 
+ * @param BAND TODO: Add defailt values for Heltec LoRa ESP32
+ * @param SS TODO: Add defailt values for Heltec LoRa ESP32
+ * @param RST TODO: Add defailt values for Heltec LoRa ESP32
+ * @param DI0 TODO: Add defailt values for Heltec LoRa ESP32
+ * @param TxPower TODO: Add defailt values for Heltec LoRa ESP32
+ */
 void ClusterDuck::setupLoRa(long BAND, int SS, int RST, int DI0, int TxPower) {
   SPI.begin(5, 19, 27, 18);
   LoRa.setPins(SS, RST, DI0);
@@ -72,6 +99,11 @@ void ClusterDuck::setupLoRa(long BAND, int SS, int RST, int DI0, int TxPower) {
 }
 
 //Setup Captive Portal
+/**
+ * @brief Setup Capative Portal
+ * 
+ * @param AP the value that will be displayed when accessing the wifi settings of devices such as smartphones and laptops.
+ */
 void ClusterDuck::setupPortal(const char *AP) {
   WiFi.mode(WIFI_AP);
   WiFi.softAP(AP);
@@ -146,7 +178,9 @@ void ClusterDuck::setupPortal(const char *AP) {
   }
 }
 
-//Setup premade DuckLink with default settings
+/**
+ * @brief Setup premade DuckLink with default settings
+ */
 void ClusterDuck::setupDuckLink() {
   setupDisplay("Duck");
   setupLoRa();
@@ -155,18 +189,27 @@ void ClusterDuck::setupDuckLink() {
   Serial.println("Duck Online");
 }
 
+/**
+ * @brief Template for running core functionality of a DuckLink.
+ */
 void ClusterDuck::runDuckLink() {
 
   processPortalRequest();
 
 }
 
+/**
+ * @brief TODO
+ */
 void ClusterDuck::processPortalRequest() {
 
   dnsServer.processNextRequest();
 
 }
 
+/**
+ * @brief Template for setting up a MamaDuck device.
+ */
 void ClusterDuck::setupMamaDuck() {
   setupDisplay("Mama");
   setupPortal();
@@ -181,6 +224,9 @@ void ClusterDuck::setupMamaDuck() {
   tymer.every(43200000, reboot);
 }
 
+/**
+ * @brief Template for running core functionality of a MamaDuck.
+ */
 void ClusterDuck::runMamaDuck() {
   tymer.tick();
 
@@ -207,6 +253,12 @@ void ClusterDuck::runMamaDuck() {
 
 }
 
+/**
+ * @brief Packages msg into a LoRa packet and sends over LoRa.
+ * Will automatically set the current device's ID as the sender ID and create a UUID for the message.
+ * 
+ * @param msg the message payload
+ */
 void ClusterDuck::sendPayloadMessage(String msg) {
   LoRa.beginPacket();
   couple(senderId_B, _deviceId);
@@ -216,6 +268,14 @@ void ClusterDuck::sendPayloadMessage(String msg) {
   LoRa.endPacket(); 
 }
 
+/**
+ * @brief Similar to and might replace sendPayloadMessage().
+ * 
+ * @param msg the message payload
+ * @param senderId the ID of the originator of the message
+ * @param messageId the UUID of the message
+ * @param path the recorded pathway of the message and is used as a check to prevent the device from sending multiple of the same message.
+ */
 void ClusterDuck::sendPayloadStandard(String msg, String senderId, String messageId, String path) {
   if(senderId == "") senderId = _deviceId;
   if(messageId == "") messageId = uuidCreator();
@@ -241,13 +301,31 @@ void ClusterDuck::sendPayloadStandard(String msg, String senderId, String messag
   Serial.println("Packet sent");
 }
 
+/**
+ * @brief Writes data to LoRa packet.   
+ * 
+ * @ref setDeviceId() for byte codes. In addition it writes the outgoing length to the LoRa packet.
+ * 
+ * Use between a LoRa.beginPacket() and LoRa.endPacket() 
+ * 
+ * @note LoRa.endPacket() will send the LoRa packet
+ * 
+ * @param byteCode byteCode is paired with the outgoing so it can be used to identify data on an individual level.
+ * @param outgoing the payload data to be sent
+ */
 void ClusterDuck::couple(byte byteCode, String outgoing) {
   LoRa.write(byteCode);               // add byteCode
   LoRa.write(outgoing.length());      // add payload length
   LoRa.print(outgoing);               // add payload
 }
 
-//Decode LoRa message
+/**
+ * @brief Decode LoRa message
+ * Used after `LoRa.read()` to convert LoRa packet into a String.
+ * 
+ * @param mLength length of the incoming message
+ * @return the converted string
+ */
 String ClusterDuck::readMessages(byte mLength)  {
   String incoming = "";
 
@@ -258,6 +336,12 @@ String ClusterDuck::readMessages(byte mLength)  {
   return incoming;
 }
 
+/**
+ * @brief Checks if the path contains deviceId. Returns bool.
+ * 
+ * @param path TODO
+ * @return true: if the path contains the deviceId
+ */
 bool ClusterDuck::idInPath(String path) {
   Serial.println("Checking Path");
   String temp = "";
@@ -282,6 +366,13 @@ bool ClusterDuck::idInPath(String path) {
   return false;
 }
 
+/**
+ * @brief Called to iterate through received LoRa packet and return data as an array of Strings.
+ * @note: if using standard byte codes it will store senderId, messageId, payload, and path in a Packet object. This can be accessed using getLastPacket()
+ * 
+ * @param pSize TODO
+ * @return TODO
+ */
 String * ClusterDuck::getPacketData(int pSize) {
   String * packetData = new String[pSize];
   int i = 0;
@@ -319,16 +410,20 @@ String * ClusterDuck::getPacketData(int pSize) {
 }
 
 /**
-  restart
-  Only restarts ESP
-*/
+ * @brief If using the ESP32 architecture, calling this function will reboot the device.
+ */
 void ClusterDuck::restartDuck()
 {
   Serial.println("Restarting Duck...");
   ESP.restart();
 }
 
-//Timer reboot
+/**
+ * @brief Used to call `restartDuck()` when using a timer
+ * @details [long description]
+ * 
+ * @return true TODO: why this is not a void function?
+ */
 bool ClusterDuck::reboot(void *) {
   String reboot = "REBOOT";
   Serial.println(reboot);
@@ -338,6 +433,11 @@ bool ClusterDuck::reboot(void *) {
   return true;
 }
 
+/**
+ * @brief Used to send a '1' over LoRa on a timer to signify the device is still on and functional.
+ * 
+ * @return true TODO: why this is not a void function?
+ */
 bool ClusterDuck::imAlive(void *) {
   String alive = "1";
   sendPayloadMessage(alive);
@@ -346,7 +446,13 @@ bool ClusterDuck::imAlive(void *) {
   return true;
 }
 
-//Get Duck MAC address
+/**
+ * @brief Returns the MAC address of the device. 
+ * @details [long description]
+ * 
+ * @param format Using `true` as an argument will return the MAC address formatted using ':'
+ * @return MAC address
+ */
 String ClusterDuck::duckMac(boolean format)
 {
   char id1[15];
@@ -380,7 +486,10 @@ String ClusterDuck::duckMac(boolean format)
   }
 }
 
-//Create a uuid
+/**
+ * @brief Create a uuid
+ * @return uuid
+ */
 String ClusterDuck::uuidCreator() {
   byte randomValue;
   char msg[50];
@@ -403,16 +512,26 @@ String ClusterDuck::uuidCreator() {
   return String(msg);
 }
 
+/**
+ * @brief TODO
+ */
 void ClusterDuck::loRaReceive() {
   LoRa.receive();
 }
 
-//Getters
-
+/**
+ * @brief Returns the device ID
+ * @return device ID
+ */
 String ClusterDuck::getDeviceId() {
   return _deviceId;
 }
 
+/**
+ * @brief Returns a packet object containing senderId, messageId, payload, and path of last packet received.
+ * @note values are updated after running `getPacketData()`
+ * @return packet object
+ */
 Packet ClusterDuck::getLastPacket() {
   Packet packet = _lastPacket;
   _lastPacket = Packet();
