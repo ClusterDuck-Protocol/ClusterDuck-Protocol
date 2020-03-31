6b93dfe8e1c055c56938057f9bfeea033fece9ee
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-03-31 23:58:40 +0200

    Update PapaDuckExample.ino

--- examples/PapaDuckExample/PapaDuckExample.ino
+++ examples/PapaDuckExample/PapaDuckExample.ino
@@ -5,13 +5,13 @@
 #include <WiFiClientSecure.h>
 #include "timer.h"
 
-#define SSID        "WIELINK-GAST"
-#define PASSWORD    "Wielink2020"
+#define SSID        ""
+#define PASSWORD    ""
 
-#define ORG         "9c6nfo"                  
-#define DEVICE_ID   "TIMO_DUCK"
+#define ORG         ""                  
+#define DEVICE_ID   ""
 #define DEVICE_TYPE "PAPA"                
-#define TOKEN       "qQTQ5q(4qvAVSlxdHu"      
+#define TOKEN       ""      
 
 char server[]           = ORG ".messaging.internetofthings.ibmcloud.com";
 char topic[]            = "iot-2/evt/status/fmt/json";
