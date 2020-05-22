7c9115980f6c915e3f937ccfb1fb05796d536142
Zach <zach@pop-os.localdomain>
2020-05-22 14:45:03 -0400

    readme pictures
    Signed-off-by: Zach <zach@pop-os.localdomain>

--- examples/PAPA_IRIDIUM/PAPA_IRIDIUM.ino
+++ examples/PAPA_IRIDIUM/PAPA_IRIDIUM.ino
@@ -31,7 +31,7 @@ void loop() {
     duck.flipInterrupt();
     int pSize = duck.handlePacket();
     if(pSize > 3) {
-      String * msg = duck.getPacketData(pSize);
+      duck.getPacketData(pSize);
       quackJson();
     }
     duck.flipInterrupt();
