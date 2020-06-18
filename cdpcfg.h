e6ae91cbfb141ee8f14536fb5efb50401a67bf0c
gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>
2020-06-18 19:21:39 +0200

    add board-specific defines for TTGO lora32 v1
    Signed-off-by: gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>

--- cdpcfg.h
+++ cdpcfg.h
@@ -31,9 +31,32 @@
    */ 
  
 
+  // BOARD "ttgo lora" not-tbeam
+  // https://github.com/Xinyuan-LilyGO/TTGO-LoRa-Series
+  #if defined(ARDUINO_TTGO_LoRa32_V1)
+    #define CDPCFG_PIN_BAT 35
+    #define CDPCFG_BAT_MULDIV 200/100
+
+    #define CDPCFG_PIN_LED1 25
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   18
+    #define CDPCFG_PIN_LORA_DIO0 26
+    #define CDPCFG_PIN_LORA_RST  23
+
+    // Oled Display settings
+    #define CDPCFG_PIN_OLED_CLOCK 22
+    #define CDPCFG_PIN_OLED_DATA  21
+
+    // actualy missing
+    #define CDPCFG_PIN_OLED_RESET -1
+    #define CDPCFG_PIN_LORA_DIO1  -1
+
+
+
   // BOARD "heltec lora v2"
   // https://heltec.org/project/wifi-lora-32/
-  #if defined(ARDUINO_HELTEC_WIFI_LORA_32_V2)
+  #elif defined(ARDUINO_HELTEC_WIFI_LORA_32_V2)
 
     #define CDPCFG_PIN_BAT  37
     #define CDPCFG_BAT_MULDIV 320/100
