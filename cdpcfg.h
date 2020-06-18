2f7f206cd7ad5c35a2379efe4024288c06e20393
gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>
2020-06-18 19:20:31 +0200

    move hardware/pin related config into board-specific block
    Signed-off-by: gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>

--- cdpcfg.h
+++ cdpcfg.h
@@ -25,63 +25,83 @@
 #ifndef CDPCFG
 #define CDPCFG
 
-// Username and Password for OTA web page
-#define CDPCFG_UPDATE_USERNAME "username"
-#define CDPCFG_UPDATE_PASSWORD "password"
+  /*
+   * HARDWARE SECTION // BOARD PINS
+   * the ARDUINO_* defs are set by the arduino build env
+   */ 
+ 
+
+  // BOARD "heltec lora v2"
+  // https://heltec.org/project/wifi-lora-32/
+  #if defined(ARDUINO_HELTEC_WIFI_LORA_32_V2)
+
+    #define CDPCFG_PIN_BAT  37
+    #define CDPCFG_BAT_MULDIV 320/100
+
+    #define CDPCFG_PIN_VEXT 21
+
+    #define CDPCFG_PIN_LED1 25
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   18
+    #define CDPCFG_PIN_LORA_DIO0 26
+    #define CDPCFG_PIN_LORA_RST  14
+
+    // Oled Display settings
+    #define CDPCFG_PIN_OLED_CLOCK 15
+    #define CDPCFG_PIN_OLED_DATA   4
+    #define CDPCFG_PIN_OLED_RESET 16
+
+    // actualy missing
+    #define CDPCFG_PIN_LORA_DIO1 -1
+
+  #else
+    #error "no known board defined"
+  #endif
 
 
-// Baud Rate
-#define CDPCFG_SERIAL_BAUD 115200
 
-// Oled Display settings
-#define CDPCFG_PIN_OLED_CLOCK 15
-#define CDPCFG_PIN_OLED_DATA   4
-#define CDPCFG_PIN_OLED_RESET 16
+  // non BOARD-specific config
 
-
-// Acces point IP adress
-#define CDPCFG_AP_IP1 192
-#define CDPCFG_AP_IP2 168
-#define CDPCFG_AP_IP3 1
-#define CDPCFG_AP_IP4 1
+  // Username and Password for OTA web page
+  #define CDPCFG_UPDATE_USERNAME "username"
+  #define CDPCFG_UPDATE_PASSWORD "password"
 
 
-// Asyncwebserver Port
-#define CDPCFG_WEB_PORT 80
+  // Serial Console Baud Rate
+  #define CDPCFG_SERIAL_BAUD 115200
 
 
-//Lora configurations
-#define CDPCFG_PIN_LORA_CS   18
-#define CDPCFG_PIN_LORA_DIO0 26
-#define CDPCFG_PIN_LORA_DIO1 25
-#define CDPCFG_PIN_LORA_RST  14
+  // Access point IP adress
+  #define CDPCFG_AP_IP1 192
+  #define CDPCFG_AP_IP2 168
+  #define CDPCFG_AP_IP3 1
+  #define CDPCFG_AP_IP4 1
 
 
-#define CDPCFG_RF_LORA_FREQ  915.0
-#define CDPCFG_RF_LORA_BW    125.0
-#define CDPCFG_RF_LORA_SF      7
-#define CDPCFG_RF_LORA_TXPOW  20
-#define CDPCFG_RF_LORA_GAIN    0
+  // Asyncwebserver Port
+  #define CDPCFG_WEB_PORT 80
+
+  // Lora RF configuration
+  #define CDPCFG_RF_LORA_FREQ  915.0
+  #define CDPCFG_RF_LORA_BW    125.0
+  #define CDPCFG_RF_LORA_SF      7
+  #define CDPCFG_RF_LORA_TXPOW  20
+  #define CDPCFG_RF_LORA_GAIN    0
+
+  // cdp configuration
+  #define CDPCFG_CDP_BUFSIZE 250
+  #define CDPCFG_UUID_LEN 8
+
+  // Timer in milliseconds
+  #define CDPCFG_MILLIS_ALIVE   1800000
+  #define CDPCFG_MILLIS_REBOOT 43200000
 
 
-#define CDPCFG_PIN_LED1 25
-
-
-#define CDPCFG_CDP_BUFSIZE 250
-
-//Timer in milliseconds
-#define CDPCFG_MILLIS_ALIVE   1800000
-#define CDPCFG_MILLIS_REBOOT 43200000
-
-//length of the uuid
-#define CDPCFG_UUID_LEN 8
-
-
-//Led Pins
-#define CDPCFG_PIN_RGBLED_R 25
-#define CDPCFG_PIN_RGBLED_G  4
-#define CDPCFG_PIN_RGBLED_B  2
-
+  // RGB Led Pins
+  #define CDPCFG_PIN_RGBLED_R 25
+  #define CDPCFG_PIN_RGBLED_G  4
+  #define CDPCFG_PIN_RGBLED_B  2
 
 
 #endif // CDPCFG
@@ -94,4 +114,6 @@
     #include "cdpcfg-post.h"
   #endif
 #endif
+
+
 
