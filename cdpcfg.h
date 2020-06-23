df55b1607853cf2510cbab5e8b9e570e5d68f83a
gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>
2020-06-23 10:53:31 +0200

    add support for board type "heltec wireless stick"
    Signed-off-by: gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>

--- cdpcfg.h
+++ cdpcfg.h
@@ -111,6 +111,37 @@
     // actualy missing
     #define CDPCFG_PIN_LORA_DIO1 -1
 
+
+
+
+  /*
+   * BOARD "heltec wireless stick"
+   * http://www.heltec.cn/project/wireless-stick/
+   * pio: board = heltec_wireless_stick
+   */
+  #elif defined(ARDUINO_HELTEC_WIRELESS_STICK)
+
+    #define CDPCFG_PIN_BAT  37
+    #define CDPCFG_BAT_MULDIV 320/100
+
+    #define CDPCFG_PIN_VEXT 21
+
+    #define CDPCFG_PIN_LED1 25
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   18
+    #define CDPCFG_PIN_LORA_DIO0 26
+    #define CDPCFG_PIN_LORA_RST  14
+
+    // Oled Display settings
+    #define CDPCFG_OLED_64x32
+    #define CDPCFG_PIN_OLED_CLOCK 15
+    #define CDPCFG_PIN_OLED_DATA   4
+    #define CDPCFG_PIN_OLED_RESET 16
+
+    // actualy missing
+    #define CDPCFG_PIN_LORA_DIO1 -1
+
   #else
     #error "no known board defined"
   #endif
