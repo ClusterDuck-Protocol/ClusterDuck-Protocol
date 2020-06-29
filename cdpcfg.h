fcfbb09ddb1ae9913ce4b4f2554c3bc7c3487bfb
gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>
2020-06-29 16:29:24 +0200

    add support for "sparkfun lora gateway 1-channel", "pycom lopy" and "pycom lopy4"
    Signed-off-by: gozu42 <tvguho-tbmh42-tct.76b2756e@hashmail.org>

--- cdpcfg.h
+++ cdpcfg.h
@@ -142,6 +142,84 @@
     // actualy missing
     #define CDPCFG_PIN_LORA_DIO1 -1
 
+
+
+  /*
+   * BOARD "sparkfun lora gateway 1-channel"
+   * https://www.sparkfun.com/products/15006
+   * pio: board = sparkfun_lora_gateway_1-channel
+   */
+  #elif defined(SPARKFUN_LGW1C)
+    // this is not an official ARDUINO_* define since this board doesnt have one
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   16
+    #define CDPCFG_PIN_LORA_DIO0 26
+    #define CDPCFG_PIN_LORA_DIO1 33
+    #define CDPCFG_PIN_LORA_DIO2 32
+    #define CDPCFG_PIN_LORA_RST   5
+
+    // Oled Display settings
+    #define CDPCFG_OLED_NONE
+
+
+
+  /*
+   * BOARD "pycom lopy"
+   * https://docs.pycom.io/datasheets/development/lopy/
+   * pio: board = lopy
+   */
+  #elif defined(ARDUINO_LoPy)
+
+    #define CDPCFG_PIN_ANT 16
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   17
+    #define CDPCFG_PIN_LORA_DIO0 23
+    #define CDPCFG_PIN_LORA_RST  18
+
+    //special SPI
+    #define CDPCFG_PIN_LORA_SPI_SCK   5
+    #define CDPCFG_PIN_LORA_SPI_MISO 19
+    #define CDPCFG_PIN_LORA_SPI_MOSI 27
+    #define CDPCFG_LORA_CLASS SX1272
+
+    // Oled Display settings
+    #define CDPCFG_OLED_NONE
+
+    // actualy missing
+    #define CDPCFG_PIN_LORA_DIO1 -1
+
+
+
+  /*
+   * BOARD "pycom lopy4"
+   * https://docs.pycom.io/datasheets/development/lopy4/
+   * pio: board = lopy4
+   */
+  #elif defined(ARDUINO_LoPy4)
+
+    #define CDPCFG_PIN_ANT 16
+
+    //Lora configurations
+    #define CDPCFG_PIN_LORA_CS   18
+    #define CDPCFG_PIN_LORA_DIO0 23
+    #define CDPCFG_PIN_LORA_RST  -1
+
+    //special SPI
+    #define CDPCFG_PIN_LORA_SPI_SCK   5
+    #define CDPCFG_PIN_LORA_SPI_MISO 19
+    #define CDPCFG_PIN_LORA_SPI_MOSI 27
+    #define CDPCFG_LORA_CLASS SX1276
+
+    // Oled Display settings
+    #define CDPCFG_OLED_NONE
+
+    // actualy missing
+    #define CDPCFG_PIN_LORA_DIO1 -1
+
+
+
   #else
     #error "no known board defined"
   #endif
