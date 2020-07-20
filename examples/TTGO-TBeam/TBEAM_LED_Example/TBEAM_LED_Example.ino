5d78a81310279d5b8e9e84c2ad9589946f2c7468
TimoWielink <timowielink@gmail.com>
2020-07-20 13:03:58 +0200

    add FastLED lib

--- examples/TTGO-TBeam/TBEAM_LED_Example/TBEAM_LED_Example.ino
+++ examples/TTGO-TBeam/TBEAM_LED_Example/TBEAM_LED_Example.ino
@@ -1,5 +1,12 @@
 #include <ClusterDuck.h>
 //#include "timer.h"
+#include "FastLED.h"
+
+// Setup for The W2812 LED
+#define LED_TYPE NEOPIXEL   // See FastLED library for all the different leds supprt 
+#define DATA_PIN 4         // Pin on the board for connecting data in for LED
+#define NUM_LEDS 1        // Nuber of LEDS on your Led Strip (use 1 for single LED)
+CRGB leds[NUM_LEDS];  
 
 //auto timer = timer_create_default(); // create a timer with default settings
 
@@ -9,9 +16,10 @@ void setup() {
   
   // put your setup code here, to run once:
   duck.begin();
-  duck.setDeviceId("R");
+  duck.setDeviceId("BETA");
   duck.setupMamaDuck();
 
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // Setup LED
   //timer.every(300000, runSensor);
 }
 
@@ -20,6 +28,8 @@ void loop() {
   
   // put your main code here, to run repeatedly:
   duck.runMamaDuck();
+  leds[0] = CRGB::Red;
+  FastLED.show();
   
   
 }
