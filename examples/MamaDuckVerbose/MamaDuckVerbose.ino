e2206c2642dfd0e9a74f8a9c0a0285fcfb73600e
Nick Feuer <nickfeuer@gmail.com>
2020-09-05 15:49:17 -0400

    add spacing to comments

--- examples/MamaDuckVerbose/MamaDuckVerbose.ino
+++ examples/MamaDuckVerbose/MamaDuckVerbose.ino
@@ -10,18 +10,25 @@ void setup() {
   // put your setup code here, to run once:
   //Begin serial for debug
   duck.begin();
+
   //Set device id, each device must have a unique id in a network
   duck.setDeviceId("R");
+
   //Setup display if there is a led screen, takes a string to display device type
   duck.setupDisplay("Mama");
+
   //Setup lora
   duck.setupLoRa();
+
   //Sets up wifi access point
   duck.setupWifiAp();
+
   //Setup DNS and IP
   duck.setupDns();
+
   //Setup web server and captive portal
   duck.setupWebServer(true);
+
   //Initialize ArduinoOTA for over the air updates
   duck.setupOTA();
 
@@ -37,33 +44,54 @@ void loop() {
   
   //Handle OTA requests
   ArduinoOTA.handle();
+  
+  //Get flag if LoRa packet received
+  if(duck.getFlag()) {
 
-  if(duck.getFlag()) {  //Get flag if LoRa packet received
-    duck.flipFlag(); //Flip lora flag for next message
-    duck.flipInterrupt(); //Flip interrupt to false
-    int pSize = duck.handlePacket(); //Read and store lora packet
+    //Flip lora flag for next message
+    duck.flipFlag(); 
+
+    //Flip interrupt to false
+    duck.flipInterrupt(); 
+
+    //Read and store lora packet
+    int pSize = duck.handlePacket(); 
     Serial.print("runMamaDuck pSize ");
     Serial.println(pSize);
-    if(pSize > 0) { //Make sure the packet has data
-      String msg = duck.getPacketData(pSize); //Decode packet data and store in _lastPacket
-      Packet lastPacket = duck.getPacketData(); //Get _lastPacket data
-      if(msg != "ping" && !duck.idInPath(lastPacket.path)) { //Check if device ID is in path
+
+    //Make sure the packet has data
+    if(pSize > 0) { 
+
+      //Decode packet data and store in _lastPacket
+      String msg = duck.getPacketData(pSize); 
+
+      //Get _lastPacket data
+      Packet lastPacket = duck.getPacketData(); 
+
+      //Check if device ID is in path
+      if(msg != "ping" && !duck.idInPath(lastPacket.path)) { 
         Serial.println("runMamaDuck relayPacket");
-        duck.sendPayloadStandard(lastPacket.payload, lastPacket.topic, lastPacket.senderId, lastPacket.messageId, lastPacket.path); //Transmit received packet
+
+        //Transmit received packet
+        duck.sendPayloadStandard(lastPacket.payload, lastPacket.topic, lastPacket.senderId, lastPacket.messageId, lastPacket.path); 
 
       }
     }
 
-    duck.flipInterrupt(); //Flip interrupt to true
+    //Flip interrupt to true
+    duck.flipInterrupt(); 
     Serial.println("runMamaDuck startReceive");
-    duck.startReceive(); //Turn on Receiver
-  }
 
+    //Turn on Receiver
+    duck.startReceive(); 
+  }
+  //Handle submits on captive portal
   duck.processPortalRequest();
   
   
 }
 
+//Use this to run code on timer
 bool runSensor(void *) {
 
   return true;
