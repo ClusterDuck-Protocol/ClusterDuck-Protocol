80172a07acfce7d41b93a17ac95e5169fbe755c6
Amir Nathoo <amir.nth@gmail.com>
2020-09-09 20:31:03 -0700

    Added a duck builder interface
    Signed-off-by: Amir Nathoo <amir.nth@gmail.com>

--- src/ClusterDuck.h
+++ src/ClusterDuck.h
@@ -31,12 +31,15 @@ public:
   ~ClusterDuck() {
     delete _duckDisplay;
     delete _duckLed;
+    delete _duckLora;
+    delete _duckNet;
   };
+  
+  friend class DuckBuilder;
 
   // Public APIs
-
+  
   // Common Duck
-  //-------------------------------------------------------------------------
   void setDeviceId(String deviceId = "");
   void begin(int baudRate = CDPCFG_SERIAL_BAUD);
   String duckMac(boolean format);
@@ -51,22 +54,18 @@ public:
   void ping();
 
   // Duck Link
-  //-------------------------------------------------------------------------
   void setupDuckLink();
   void runDuckLink();
 
   // Mama Duck
-  //-------------------------------------------------------------------------
   void setupMamaDuck();
   void runMamaDuck();
 
   // Detect Duck
-  //-------------------------------------------------------------------------
   void setupDetect();
   int runDetect();
 
   // LoRa
-  //-------------------------------------------------------------------------
   void setupLoRa(long BAND = CDPCFG_RF_LORA_FREQ, int SS = CDPCFG_PIN_LORA_CS,
                  int RST = CDPCFG_PIN_LORA_RST, int DI0 = CDPCFG_PIN_LORA_DIO0,
                  int DI1 = CDPCFG_PIN_LORA_DIO1,
@@ -85,11 +84,9 @@ public:
   int getRSSI();
 
   // Display
-  //-------------------------------------------------------------------------
   void setupDisplay(String deviceType);
 
   // Network
-  //-------------------------------------------------------------------------
   void setupWebServer(bool createCaptivePortal = false);
   void setupWifiAp(const char* AP = " 🆘 DUCK EMERGENCY PORTAL");
   void setupDns();
@@ -101,18 +98,15 @@ public:
   String getPassword();
 
   // OTA - Firmware upgrade
-  //-------------------------------------------------------------------------
   void setupOTA();
 
   // Portal
-  //-------------------------------------------------------------------------
   void processPortalRequest();
   String* getPortalDataArray();
   String getPortalDataString();
   bool runCaptivePortal();
 
   // Led
-  //-------------------------------------------------------------------------
   void setColor(int ledR = CDPCFG_PIN_RGBLED_R, int ledG = CDPCFG_PIN_RGBLED_G,
                 int ledB = CDPCFG_PIN_RGBLED_B);
   void setupLED();
@@ -144,6 +138,83 @@ private:
 
   static String runTime;
 };
+
+class DuckBuilder {
+public:
+  DuckBuilder(){
+  }
+
+  auto createAs(String id) -> DuckBuilder& {
+    _duck.begin();
+    _duck.setDeviceId(id);
+    return *this;
+  }
+
+  auto createMamaDuckAs(String id) -> ClusterDuck& {
+    _duck.begin();
+    _duck.setDeviceId(id);
+    _duck.setupMamaDuck();
+    return _duck;
+  }
+  auto createDuckLinkAs(String id) -> ClusterDuck& {
+    _duck.begin();
+    _duck.setDeviceId(id);
+    _duck.setupDuckLink();
+    return _duck;
+  }
+
+  auto createDectorDuckAs(String id) -> ClusterDuck& {
+    _duck.begin();
+    _duck.setDeviceId(id);
+    _duck.setupDetect();
+    return _duck;
+  }
+  auto withLora(long BAND = CDPCFG_RF_LORA_FREQ, int SS = CDPCFG_PIN_LORA_CS,
+                int RST = CDPCFG_PIN_LORA_RST, int DI0 = CDPCFG_PIN_LORA_DIO0,
+                int DI1 = CDPCFG_PIN_LORA_DIO1,
+                int TxPower = CDPCFG_RF_LORA_TXPOW) -> DuckBuilder& {
+    _duck.setupLoRa(BAND, SS, RST, DI0, DI1, TxPower);
+    return *this;
+  }
+
+  auto withDisplay(String deviceType) -> DuckBuilder& {
+    _duck.setupDisplay(deviceType);
+    return *this;
+  }
+
+  auto withWifi(const char* ap = " 🆘 DUCK EMERGENCY PORTAL") -> DuckBuilder& {
+    _duck.setupWifiAp(ap);
+    return *this;
+  }
+
+  auto withDns() -> DuckBuilder& {
+    _duck.setupDns();
+    return *this;
+  }
+
+  auto withInternet(String ssid, String password) -> DuckBuilder& {
+    _duck.setupInternet(ssid, password);
+    return *this;
+  }
+
+  auto withOta() -> DuckBuilder& {
+    _duck.setupOTA();
+    return *this;
+  }
+
+  auto withWebServer(bool createCaptivePortal = false) -> DuckBuilder& {
+    _duck.setupWebServer(createCaptivePortal);
+    return *this;
+  }
+  auto withLed()->DuckBuilder& {
+    _duck.setupLED();
+    return *this;
+  }
+  auto done() -> ClusterDuck& { return _duck; }
+
+private:
+  ClusterDuck _duck;
+  };
 
 class CaptiveRequestHandler : public AsyncWebHandler {
 public:
