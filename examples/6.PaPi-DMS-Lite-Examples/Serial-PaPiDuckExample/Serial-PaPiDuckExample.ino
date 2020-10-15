b585d5eca3f9e42ef4a200e0f9b449f9a4dccf43
TimoWielink <timowielink@gmail.com>
2020-10-15 16:21:58 +0200

    update examples

--- examples/6.PaPi-DMS-Lite-Examples/Serial-PaPiDuckExample/Serial-PaPiDuckExample.ino
+++ examples/6.PaPi-DMS-Lite-Examples/Serial-PaPiDuckExample/Serial-PaPiDuckExample.ino
@@ -1,68 +1,81 @@
-#include <ClusterDuck.h>
-
+/**
+ * @file papa-duck-with-callback.ino
+ * @author 
+ * @brief Uses built-in PapaDuck from the SDK to create a WiFi enabled Papa Duck
+ * 
+ * This example will configure and run a Papa Duck that connects to the cloud
+ * and forwards all messages (except  pings) to the cloud.
+ * 
+ * @date 2020-09-21
+ * 
+ */
+#include <PapaDuck.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+
 #include "timer.h"
 
+#define LORA_FREQ 915.0 // Frequency Range. Set for US Region 915.0Mhz
+#define LORA_TXPOWER 20 // Transmit Power
+// LORA HELTEC PIN CONFIG
+#define LORA_CS_PIN 18
+#define LORA_DIO0_PIN 26
+#define LORA_DIO1_PIN -1 // unused
+#define LORA_RST_PIN 14
+
+// Use pre-built papa duck: the duck ID is provided by DMS
+PapaDuck duck = PapaDuck("PAPA");
+ 
 
 
-ClusterDuck duck;
 
-auto timer = timer_create_default(); // create a timer with default settings
-
-
-byte ping = 0xF4;
+bool retry = true;
 
 void setup() {
-  // put your setup code here, to run once:
+  duck.setupSerial(115200);
+  duck.setupRadio(LORA_FREQ, LORA_CS_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_DIO1_PIN, LORA_TXPOWER);
+  
+  // register a callback to handle incoming data from duck in the network
+  duck.onReceiveDuckData(handleDuckData);
+  
+  Serial.println("[PAPA] Setup OK!");
+}
 
-  duck.begin();
-  duck.setDeviceId("Papa");
-
-  duck.setupLoRa();
-  duck.setupDisplay("Papa");
-
-  Serial.println("PAPA Online");
+// The callback method simply takes the incoming packet and
+// converts it to a JSON string, before sending it out over WiFi
+void handleDuckData(Packet packet) {
+  quackJson(packet);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  if(duck.getFlag()) {  //If LoRa packet received
-    duck.flipFlag();
-    duck.flipInterrupt();
-    int pSize = duck.handlePacket();
-    if(pSize > 3) {
-      duck.getPacketData(pSize);
-      quackJson();
-
-    }
-    duck.flipInterrupt();
-    duck.startReceive();
-  }
 
 
-  timer.tick();
+  duck.run();
+
 }
 
-void quackJson() {
-  const int bufferSize = 4*  JSON_OBJECT_SIZE(4);
+
+void quackJson(Packet packet) {
+
+  if (packet.topic == "ping") {
+    return;
+  }
+  
+  const int bufferSize = 4 *  JSON_OBJECT_SIZE(4);
   StaticJsonDocument<bufferSize> doc;
 
-  JsonObject root = doc.as<JsonObject>();
+  doc["DeviceID"]  = packet.senderId;
+  doc["MessageID"] = packet.messageId;
+  doc["Payload"].set(packet.payload);
 
-  Packet lastPacket = duck.getLastPacket();
+  // FIXME: Path shouldn't be altered by the application
+  // It should done in the library PapaDuck component
+  doc["path"].set(packet.path + "," + "PAPA");
 
-  doc["DeviceID"]        = lastPacket.senderId;
-  doc["MessageID"]       = lastPacket.messageId;
-  doc["Payload"]     .set(lastPacket.payload);
-  doc["path"]         .set(lastPacket.path + "," + duck.getDeviceId());
 
 
   String jsonstat;
-  serializeJson(doc, jsonstat);
-
-  Serial.println(jsonstat);
-    
+//  serializeJson(doc, jsonstat);
+  serializeJsonPretty(doc, Serial);
 
 }
--- examples/6.PaPi-DMS-Lite-Examples/Wifi-PaPiDuckExample/WiFi-PaPiDuckExample.ino
+++ examples/6.PaPi-DMS-Lite-Examples/Wifi-PaPiDuckExample/WiFi-PaPiDuckExample.ino
@@ -6,116 +6,141 @@
 *
 */
 
-#include <ClusterDuck.h>
-
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include "timer.h"
 
-ClusterDuck duck;
+// CDP headers
+#include <PapaDuck.h>
+#include <DuckDisplay.h>
 
-auto timer = timer_create_default(); // create a timer with default settings
+// Note the device id here is just an example.
+// It should be a unique securely stored ID
+#define DEVICE_ID   "PapaDuck"
 
+// Use pre-built papa duck.
+PapaDuck duck = PapaDuck(DEVICE_ID);
+
+
+// OLED display
+DuckDisplay* display = NULL;
+
+// create a timer with default settings
+auto timer = timer_create_default();
 char topic[] = "status";
 const char* user = "raspi-webgui";
 const char* pass = "ChangeMe";
 const char* mqtt_server = "10.3.141.1";
+const int MQTT_CONNECTION_DELAY_MS = 5000;
+const int WIFI_CONNECTION_DELAY_MS = 500;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-byte ping = 0xF4;
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
 
 void setup() {
-  duck.begin();
-  duck.setDeviceId("Papa");
-
-  duck.setupLoRa();
-  duck.setupDisplay("Papa");
+  duck.setupSerial();
+  duck.setupRadio();
+  duck.onReceiveDuckData(handleDuckData);
+  // This duck has an OLED display and we want to use it. 
+  // Get an instance and initialize it, so we can use in our application
+  // Note: if a display is not available, we still get a valid DuckDisplay
+  // but the API calls will be a no-op
+  display = DuckDisplay::getInstance();
+  display->setupDisplay();
+  display->drawString(true, 10,10, "setup wifi");
   setup_wifi();
-  
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  display->drawString(true, 10,10, "setup mqtt");
+  mqttClient.setServer(mqtt_server, 1883);
+  mqttClient.setCallback(callback);
+  display->drawString(true, 10,10, "PAPA READY");
 }
+
+/**
+ * @brief Callback method invoked when a packet was received from the mesh
+ * 
+ * @param packet data packet that contains the received message
+ */
+void handleDuckData(Packet packet) {
+  quackJson(packet);
+  Serial.print("TEST");
+}
+/**
+ * @brief Establish the connection to the wifi network the Papa Duck can reach
+ * 
+ */
 void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(user);
   WiFi.begin(user, pass);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(WIFI_CONNECTION_DELAY_MS);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
 }
+/**
+ * @brief Invoked by the MQTT server when a message has been received
+ */
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
 }
-
+/**
+ * @brief Periodically attempts to re-establish the MQTT connection
+ * 
+ */
 void reconnect() {
-  while (!client.connected()) {
+  while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP32Client")) {
+    if (mqttClient.connect("ESP32Client")) {
       Serial.println("connected");
-      client.subscribe("status");
+      mqttClient.subscribe("status");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+//      Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      delay(MQTT_CONNECTION_DELAY_MS);
     }
   }
 }
 void loop() {
-  if (!client.connected()) {
+  if (!mqttClient.connected()) {
     reconnect();
   }
-  client.loop();
-  if(duck.getFlag()) {  //If LoRa packet received
-    duck.flipFlag();
-    duck.flipInterrupt();
-    int pSize = duck.handlePacket();
-    if(pSize > 3) {
-      String * msg = duck.getPacketData(pSize);
-      quackJson();
-
-    }
-    duck.flipInterrupt();
-    duck.startReceive();
-  }
+  mqttClient.loop();
+   duck.run();
 }
-void quackJson() {
+/**
+ * @brief Convert received packet into a JSON object we can send over the MQTT connection
+ * 
+ * @param packet A Packet that contains the received message
+ */
+void quackJson(Packet packet) {
+  if (packet.topic == "ping") {
+    return;
+  }
   const int bufferSize = 4*  JSON_OBJECT_SIZE(4);
   StaticJsonDocument<bufferSize> doc;
-
   JsonObject root = doc.as<JsonObject>();
-
-  Packet lastPacket = duck.getLastPacket();
-
-  doc["DeviceID"]        = lastPacket.senderId;
-  doc["MessageID"]       = lastPacket.messageId;
-  doc["Payload"]     .set(lastPacket.payload);
-  doc["path"]         .set(lastPacket.path + "," + duck.getDeviceId());
-
-
+ 
+  doc["DeviceID"]  = packet.senderId;
+  doc["MessageID"] = packet.messageId;
+  doc["Payload"].set(packet.payload);
+  // FIXME: Path shouldn't be altered by the application
+  // It should done in the library PapaDuck component
+  doc["path"].set(packet.path + "," + DEVICE_ID);
   String jsonstat;
   serializeJson(doc, jsonstat);
-
-  if (client.publish(topic, jsonstat.c_str())) {
-
+  if (mqttClient.publish(topic, jsonstat.c_str())) {
     serializeJsonPretty(doc, Serial);
-     Serial.println("");
+    Serial.println("");
     Serial.println("Publish ok");
-
   }
   else {
     Serial.println("Publish failed");
   }
-
 }
