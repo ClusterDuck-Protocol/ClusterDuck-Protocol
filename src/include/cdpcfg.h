7785e45d7917bd786c9c4f25920afb3553af80e8
Amir Nathoo <amir.nth@gmail.com>
2020-10-16 10:54:42 -0700

    added the LORA pin config back as they are used for defsult settings of the LoRa config

--- src/include/cdpcfg.h
+++ src/include/cdpcfg.h
@@ -153,6 +153,15 @@
 
 #define CDPCFG_HELTEC_CUBE_CELL
 
+// TODO: These are not used by the Heltec LoRa library
+// But they still need to be defined for now because the Duck::setupRadio() uses them.
+// Update the setupRadio to use a RadioConfig data structure instead so we don't need to expose these to the apps
+#define CDPCFG_PIN_LORA_CS 10
+#define CDPCFG_PIN_LORA_DIO0 9 // BUSY PIN
+#define CDPCFG_PIN_LORA_DIO1 8
+#define CDPCFG_PIN_LORA_DIO2 -1
+#define CDPCFG_PIN_LORA_RST 14
+
 // Oled Display settings
 #define CDPCFG_OLED_NONE
 
