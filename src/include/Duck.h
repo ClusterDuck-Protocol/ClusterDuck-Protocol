0999d7b5b159deed2074013032aec91664a226dc
Amir Nathoo <amir.nth@gmail.com>
2020-10-28 14:33:53 -0700

    Fixes broken build for no display ducks and circular deps. Added a DuckTypes for global CDP types to avoid circular deps

--- src/include/Duck.h
+++ src/include/Duck.h
@@ -10,23 +10,6 @@
 #include "DuckTypes.h"
 #include "cdpcfg.h"
 
-/**
- * @brief Type of ducks
- *
- */
-enum DuckType {
-  /// A Duck of unknown type
-  UNKNOWN = 0x00,
-  /// A PapaDuck
-  PAPA = 0x01,
-  /// A MamaDuck
-  MAMA = 0x02,
-  /// A DuckLink
-  LINK = 0x03,
-  /// A Detector Duck
-  DETECTOR = 0x04,
-  MAX_TYPE
-};
 
 class Duck {
 
