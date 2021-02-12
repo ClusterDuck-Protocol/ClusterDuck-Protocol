ebaef1392dfa2d9f5cae906784084ae4bf168aa2
nfeuer <nickfeuer@gmail.com>
2021-02-12 15:33:32 -0500

    add crypto files

---
+++ src/include/DuckCrypto.h
@@ -0,0 +1,34 @@
+/**
+ * @file DuckCrypto.h
+ * @brief This file is internal to CDP and provides the library access to
+ * encryption functions.
+ *
+ * The implementation is conditioned by the `CDPCFG_ENCRYPTION_NONE` flag which may be
+ * defined in `cdpcfh.h` to disable encryption.
+ * @version
+ * @date 2021-02-10
+ *
+ * @copyright
+ */
+
+// Crypto Libraries
+#include "../Libraries/arduinolibs/libraries/Crypto/Crypto.h"
+#include "../Libraries/arduinolibs/libraries/Crypto/AES.h"
+#include "../Libraries/arduinolibs/libraries/Crypto/CTR.h"
+#include <vector>
+#include <string>
+
+
+//class DuckCrypto {
+
+// public:
+//    int setAESKey(uint8_t newKEY[32]);
+//    int setAESIV(uint8_t newIV[16]);
+
+//    void encryptData(std::vector<uint8_t> text, std::vector<uint8_t> encryptedData, size_t inc);
+
+// private:
+//    uint8_t KEY[32];
+//    uint8_t IV[16];
+
+//}
