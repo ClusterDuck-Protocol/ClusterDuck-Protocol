d19d556b705ddb3b15c787b6dac5c2e22c296029
Ben Zimmerman <benjamin.zimmerman@ibm.com>
2021-10-14 15:06:45 -0500

    src: move EEPROM methods from DuckNet to DuckUtils
    * Move the functionality of saving the WiFi credentials to EEPROM to
    the saveWifiCredentials function in DuckUtils
    
    * Move the functionality of loading the WiFi credentials from EEPROM to
    the loadWifiSsid and loadWifiPassword functions in DuckUtils
    
    Fixes: https://github.com/Call-for-Code/ClusterDuck-Protocol/issues/231
    Signed-off-by: Ben Zimmerman <benjamin.zimmerman@ibm.com>

--- src/DuckUtils.cpp
+++ src/DuckUtils.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 
+#include "DuckLogger.h"
+
 namespace duckutils {
 
   namespace {
@@ -70,6 +72,58 @@ uint32_t toUnit32(const byte* data) {
     value |= data[2] << 8;
     value |= data[3];
     return value;
+}
+
+int saveWifiCredentials(String ssid, String password) {
+  EEPROM.begin(512);
+
+  if (ssid.length() > 0 && password.length() > 0) {
+    loginfo("Clearing EEPROM");
+    for (int i = 0; i < 96; i++) {
+      EEPROM.write(i, 0);
+    }
+
+    loginfo("writing EEPROM SSID:");
+    for (int i = 0; i < ssid.length(); i++)
+    {
+      EEPROM.write(i, ssid[i]);
+      loginfo("Wrote: ");
+      loginfo(ssid[i]);
+    }
+    loginfo("writing EEPROM Password:");
+    for (int i = 0; i < password.length(); ++i)
+    {
+      EEPROM.write(32 + i, password[i]);
+      loginfo("Wrote: ");
+      loginfo(password[i]);
+    }
+    EEPROM.commit();
+  }
+  return DUCK_ERR_NONE;
+}
+
+String loadWifiSsid() {
+  EEPROM.begin(512); //Initialasing EEPROM
+  String esid;
+  // loop through saved SSID characters
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  loginfo("Reading EEPROM SSID: " + esid);
+  return esid;
+}
+
+String loadWifiPassword() {
+  EEPROM.begin(512); //Initialasing EEPROM
+  String epass = "";
+  // loop through saved Password characters
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  loginfo("Reading EEPROM Password: " + epass);
+  return epass;
 }
 
 } // namespace duckutils
