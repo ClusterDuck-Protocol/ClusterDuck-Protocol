5038d8b268ebbec07868c1666df46ab1dc68ae1c
MrARM <jordan@thesmokinggnu.us>
2022-02-18 21:02:34 -0600

    Fix board configuration typos in TTGO T-Beam
    This addresses two fixes for the TTGO T-Beam,
    1. The compiler flag was incorrect, which caused build issues and the board defaulted to a Heltec board.
    2. The OLED SDA/SCL pins were backwards, which caused the OLED to not properly initialize.
    
    Signed-off-by: Jordan Bush <jordan@thesmokinggnu.us>

--- src/include/cdpcfg.h
+++ src/include/cdpcfg.h
@@ -92,7 +92,7 @@
 #define CDPCFG_PIN_LORA_DIO1 -1
 
 // T-Beam
- #elif defined(ARDUINO_TBeam)
+ #elif defined(ARDUINO_T_Beam)
   #define CDPCFG_PIN_BAT 35 
   #define CDPCFG_BAT_MULDIV 200 / 100 
   #define CDPCFG_PIN_LED1 25 
@@ -101,8 +101,8 @@
   #define CDPCFG_PIN_LORA_DIO0 26 
   #define CDPCFG_PIN_LORA_RST 14 
   // Oled Display settings 
-  #define CDPCFG_PIN_OLED_CLOCK 21
-  #define CDPCFG_PIN_OLED_DATA 22
+  #define CDPCFG_PIN_OLED_CLOCK 22
+  #define CDPCFG_PIN_OLED_DATA 21
   #define CDPCFG_PIN_OLED_RESET 16 
   #define CDPCFG_PIN_OLED_ROTATION U8G2_R0 
   // actualy missing 
