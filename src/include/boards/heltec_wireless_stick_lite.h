e0b7c749b73f99cbaef2c7bd0e66f99b81a991f7
Lennart Buhl <git@hackmate.de>
2022-05-20 22:06:59 +0200

    Split board config into seperate files
    Signed-off-by: Lennart Buhl <git@hackmate.de>

---
+++ src/include/boards/heltec_wireless_stick_lite.h
@@ -0,0 +1,15 @@
+#define CDPCFG_PIN_BAT 37
+#define CDPCFG_BAT_MULDIV 320 / 100
+
+#define CDPCFG_PIN_VEXT 21
+
+#define CDPCFG_PIN_LED1 25
+
+// LoRa configuration
+#define CDPCFG_PIN_LORA_CS 18
+#define CDPCFG_PIN_LORA_DIO0 26
+#define CDPCFG_PIN_LORA_DIO1 -1
+#define CDPCFG_PIN_LORA_RST 14
+
+// OLED display settings
+#define CDPCFG_OLED_NONE
