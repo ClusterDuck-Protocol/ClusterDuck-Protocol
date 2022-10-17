7cd6e00b34cdcece2a42cb0d85575ff53cc79d7b
Brenton <brentonpoke@outlook.com>
2022-10-17 16:47:31 -0400

    missed this too
    Signed-off-by: Brenton <brentonpoke@outlook.com>

--- src/Ducks/MamaDuck.cpp
+++ src/Ducks/MamaDuck.cpp
@@ -271,7 +271,7 @@ void MamaDuck::handleAck(const CdpPacket & packet) {
         case DeserializationError::Ok: {
             lastMessageAck = false;
             for(int i=0; i < MAX_MUID_PER_ACK;i++) {
-                if (duckutils::toString(lastMessageMuid) == doc["pairs"][i]) {
+                if (lastMessageMuid == doc["pairs"][i]) {
                     loginfo("Message acked at time: "+ String((int)doc["txTime"]));
                     lastMessageAck = true;
                 }
