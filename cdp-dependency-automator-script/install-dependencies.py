8c9f6db53dfc184f98afc98c4af8ee906ce6129e
faradaym <rcheyenne.truss@gmail.com>
2024-01-04 21:01:21 -0600

    script improvements
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>

--- cdp-dependency-automator-script/install-dependencies.py
+++ cdp-dependency-automator-script/install-dependencies.py
@@ -1,61 +1,88 @@
+#!/usr/bin/python3
 import os
 import requests
 import json
 import zipfile
+import sys
 
-retry_count = 0
-json_file = open('dependencies.json')
-dependencies = json.load(json_file)
+def run():
+    destination = os.path.dirname(__file__)
+    while os.path.basename(destination) != 'libraries':
+        destination = os.path.dirname(destination)
 
-absolute_path = os.path.dirname(__file__)
-destination = absolute_path
-while os.path.basename(destination) != 'libraries':
-    destination = os.path.dirname(destination)
+    if not os.path.exists(destination):
+        os.makedirs(destination)
 
-if not os.path.exists(destination):
-    os.makedirs(destination)
+    print(f"installing basic CDP dependencies in {destination}")
+    install('dependencies.json', destination)
 
-for dependency, info in dependencies.items():
-    MAX_RETRIES = 5
-    while retry_count < 5:
-        try:
-            response = requests.get(info["url"])
+    prompting = True
+    while prompting:
+        print(f"install optional dependencies for sensors, gps, etc? (Y)es/(N)o")
+        choice = input().lower()
+        if choice == '' or choice in ["yes", "ye", "y"]:
+            install('optional-dependencies.json', destination)
+            print("Installation complete.")
+            prompting = False
+        elif choice in ["no", "n"]:
+            prompting = False
+            print("Installation complete.")
+        else:
+            print("Please respond with 'yes' or 'no'")
 
-            if response.status_code == 200:
-                content_type = response.headers.get('Content-Type')
-                if content_type == 'application/zip':
-                    file_path = os.path.join(destination, dependency)
 
-                    with open(file_path, 'wb') as file:
-                        file.write(response.content)
+def install(dep_file_name, destination): #update to use brentons dep version
+    retry_count = 0
+    json_file = open(dep_file_name)
+    dependencies = json.load(json_file)
 
-                    print(f"{dependency} successfully downloaded.")
+    for dependency, info in dependencies.items():
+        MAX_RETRIES = 5
+        while retry_count < 5:
+            try:
+                response = requests.get(info["url"])
 
-                    print(f"Unzipping...")
-                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                        zip_ref.extractall(destination)
+                if response.status_code == 200:
+                    content_type = response.headers.get('Content-Type')
+                    if content_type == 'application/zip':
+                        file_path = os.path.join(destination, dependency)
 
-                    os.remove(file_path)
+                        with open(file_path, 'wb') as file:
+                            file.write(response.content)
 
-                elif content_type == 'application/octet-stream':
-                    file_path = os.path.join(destination, dependency)
-                    if not os.path.exists(file_path):
-                        os.makedirs(file_path)
+                        print(f"{dependency} v{info['version']} successfully downloaded.")
 
-                    with open(os.path.join(file_path, dependency) + ".h", 'wb') as file:
-                        file.write(response.content)
+                        print(f"Unzipping...")
+                        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                            zip_ref.extractall(destination)
 
-                    print(f"{dependency} successfully downloaded.")
+                        os.remove(file_path)
 
-                else:
-                    print(f"{dependency} -- unsupported content type: {content_type}")
+                    elif content_type == 'application/octet-stream':
+                        file_path = os.path.join(destination, dependency)
+                        if not os.path.exists(file_path):
+                            os.makedirs(file_path)
 
-            break
+                        with open(os.path.join(file_path, dependency) + ".h", 'wb') as file:
+                            file.write(response.content)
 
-        except requests.exceptions.RequestException as e:
-            print(f"Error downloading {dependency}: {e}")
-            retry_count += 1
-            if retry_count == MAX_RETRIES:
-                print(f"Failed to download {dependency}.")
-                retry_count = 0
+                        print(f"{dependency} successfully downloaded.")
+
+                    else:
+                        print(f"{dependency} -- unsupported content type: {content_type}")
+
                 break
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error downloading {dependency}: {e}")
+                retry_count += 1
+                if retry_count == MAX_RETRIES:
+                    print(f"Failed to download {dependency}.")
+                    retry_count = 0
+                    break
+
+#enforce use of python 3
+if sys.version_info[0] < 3:
+   print(f"Script requires python 3 -- your version: {sys.version_info[0]}")
+else:
+    run()
