339b5986a935bd005693c99071e035d9965e2089
Brenton Poke <brentonpoke@outlook.com>
2024-02-03 06:03:04 -0500

    almost there
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- src/DuckGPS.h
+++ src/DuckGPS.h
@@ -14,11 +14,15 @@ class DuckGPS {
 public:
 #if defined(CDPCFG_GPS_RX) && defined(CDPCFG_GPS_TX)
     DuckGPS() : GPSSerial(1) {
-        GPSSerial.begin(9600, SERIAL_8N1,CDPCFG_GPS_RX,CDPCFG_GPS_TX);
+        GPSSerial.begin(9600, SERIAL_8N1, CDPCFG_GPS_RX, CDPCFG_GPS_TX);
     };
 #endif
-    DuckGPS(int RX, int TX) : GPSSerial(1) {
-        GPSSerial.begin(9600, SERIAL_8N1, RX, TX);
+    DuckGPS(int Rx, int Tx) : GPSSerial(1) {
+        GPSSerial.begin(9600, SERIAL_8N1, Rx, Tx);
+
+
+    };
+    void setup(){
         memset(&info, 0, sizeof(struct GnssModelInfo));
         uint8_t buffer[768] = {0};
         delay(100);
@@ -51,7 +55,7 @@ public:
                 // commands
 
             }
-            delay(750);
+            delay(850);
 
             msglen = makeUBXPacket(0x06, 0x39, sizeof(_message_JAM), _message_JAM);
             GPSSerial.write(UBXscratch, msglen);
@@ -72,45 +76,44 @@ public:
             if (getACK(0x06, 0x08, 300) != GNSS_RESPONSE_OK) {
                 logwarn_f("Unable to set GPS update rate.\n");
             }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GGL), _message_GGL);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to disable NMEA GGL.\n");
-            }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GSA), _message_GSA);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to Enable NMEA GSA.\n");
-            }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GSV), _message_GSV);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to disable NMEA GSV.\n");
-            }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_VTG), _message_VTG);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to disable NMEA VTG.\n");
-            }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_RMC), _message_RMC);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to enable NMEA RMC.\n");
-            }
-
-            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GGA), _message_GGA);
-            GPSSerial.write(UBXscratch, msglen);
-            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
-                logwarn_f("Unable to enable NMEA GGA.\n");
-            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GGL), _message_GGL);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to disable NMEA GGL.\n");
+//            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GSA), _message_GSA);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to Enable NMEA GSA.\n");
+//            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GSV), _message_GSV);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to disable NMEA GSV.\n");
+//            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_VTG), _message_VTG);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to disable NMEA VTG.\n");
+//            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_RMC), _message_RMC);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to enable NMEA RMC.\n");
+//            }
+//
+//            msglen = makeUBXPacket(0x06, 0x01, sizeof(_message_GGA), _message_GGA);
+//            GPSSerial.write(UBXscratch, msglen);
+//            if (getACK(0x06, 0x01, 300) != GNSS_RESPONSE_OK) {
+//                logwarn_f("Unable to enable NMEA GGA.\n");
+//            }
         }
-
-    };
+    }
     enum AltitudeUnit{
         meter,
         kilo,
@@ -214,17 +217,108 @@ private:
         sfe_ublox_packet_validity_e valid;           // Goes from NOT_DEFINED to VALID or NOT_VALID when checksum is checked
         sfe_ublox_packet_validity_e classAndIDmatch; // Goes from NOT_DEFINED to VALID or NOT_VALID when the Class and ID match the requestedClass and requestedID
     };
-    uint8_t _message_GNSS_7[200];
-    uint8_t _message_GNSS[200];
-    uint8_t _message_JAM[200];
-    uint8_t _message_NAVX5[200];
-    uint8_t _message_1HZ[200];
-    uint8_t _message_GGL[200];
-    uint8_t _message_GSA[200];
-    uint8_t _message_GSV[200];
-    uint8_t _message_VTG[200];
-    uint8_t _message_RMC[200];
-    uint8_t _message_GGA[200];
+    const uint8_t _message_GNSS_7[200] = {
+            0x00, // msgVer (0 for this version)
+            0x00, // numTrkChHw (max number of hardware channels, read only, so it's always 0)
+            0xff, // numTrkChUse (max number of channels to use, 0xff = max available)
+            0x02, // numConfigBlocks (number of GNSS systems), most modules support maximum 3 GNSS systems
+            // GNSS config format: gnssId, resTrkCh, maxTrkCh, reserved1, flags
+            0x00, 0x08, 0x10, 0x00, 0x01, 0x00, 0x00, 0x01, // GPS
+            0x01, 0x01, 0x03, 0x00, 0x01, 0x00, 0x00, 0x01  // SBAS
+    };
+    const uint8_t _message_GNSS[200] = {
+            0x00, // msgVer (0 for this version)
+            0x00, // numTrkChHw (max number of hardware channels, read only, so it's always 0)
+            0xff, // numTrkChUse (max number of channels to use, 0xff = max available)
+            0x03, // numConfigBlocks (number of GNSS systems), most modules support maximum 3 GNSS systems
+            // GNSS config format: gnssId, resTrkCh, maxTrkCh, reserved1, flags
+            0x00, 0x08, 0x10, 0x00, 0x01, 0x00, 0x01, 0x01, // GPS
+            0x01, 0x01, 0x03, 0x00, 0x01, 0x00, 0x01, 0x01, // SBAS
+            0x06, 0x08, 0x0e, 0x00, 0x01, 0x00, 0x01, 0x01  // GLONASS
+    };
+    const uint8_t _message_JAM[200] = {
+            // bbThreshold (Broadband jamming detection threshold) is set to 0x3F (63 in decimal)
+            // cwThreshold (CW jamming detection threshold) is set to 0x10 (16 in decimal)
+            // algorithmBits (Reserved algorithm settings) is set to 0x16B156 as recommended
+            // enable (Enable interference detection) is set to 1 (enabled)
+            0x3F, 0x10, 0xB1, 0x56, // config: Interference config word
+            // generalBits (General settings) is set to 0x31E as recommended
+            // antSetting (Antenna setting, 0=unknown, 1=passive, 2=active) is set to 0 (unknown)
+            // ToDo: Set to 1 (passive) or 2 (active) if known, for example from UBX-MON-HW, or from board info
+            // enable2 (Set to 1 to scan auxiliary bands, u-blox 8 / u-blox M8 only, otherwise ignored) is set to 1
+            // (enabled)
+            0x1E, 0x03, 0x00, 0x01 // config2: Extra settings for jamming/interference monitor
+    };
+    const uint8_t _message_NAVX5[200] = {
+            0x00, 0x00, // msgVer (0 for this version)
+            // minMax flag = 1: apply min/max SVs settings
+            // minCno flag = 1: apply minimum C/N0 setting
+            // initial3dfix flag = 0: apply initial 3D fix settings
+            // aop flag = 1: apply aopCfg (useAOP flag) settings (AssistNow Autonomous)
+            0x1B, 0x00, // mask1 (First parameters bitmask)
+            // adr flag = 0: apply ADR sensor fusion on/off setting (useAdr flag)
+            // If firmware is not ADR/UDR, enabling this flag will fail configuration
+            // ToDo: check this with UBX-MON-VER
+            0x00, 0x00, 0x00, 0x00,             // mask2 (Second parameters bitmask)
+            0x00, 0x00,                         // Reserved
+            0x03,                               // minSVs (Minimum number of satellites for navigation) = 3
+            0x10,                               // maxSVs (Maximum number of satellites for navigation) = 16
+            0x06,                               // minCNO (Minimum satellite signal level for navigation) = 6 dBHz
+            0x00,                               // Reserved
+            0x00,                               // iniFix3D (Initial fix must be 3D) = 0 (disabled)
+            0x00, 0x00,                         // Reserved
+            0x00,                               // ackAiding (Issue acknowledgements for assistance message input) = 0 (disabled)
+            0x00, 0x00,                         // Reserved
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Reserved
+            0x00,                               // Reserved
+            0x01,                               // aopCfg (AssistNow Autonomous configuration) = 1 (enabled)
+            0x00, 0x00,                         // Reserved
+            0x00, 0x00,                         // Reserved
+            0x00, 0x00, 0x00, 0x00,             // Reserved
+            0x00, 0x00, 0x00,                   // Reserved
+            0x01,                               // useAdr (Enable/disable ADR sensor fusion) = 1 (enabled)
+    };
+    const uint8_t _message_1HZ[200] = {
+            0xE8, 0x03, // Measurement Rate (1000ms for 1Hz)
+            0x01, 0x00, // Navigation rate, always 1 in GPS mode
+            0x01, 0x00, // Time reference
+    };
+    const uint8_t _message_GGL[200] =  {
+            0xF0, 0x01,            // NMEA ID for GLL
+            0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+            0x00,                  // Disable
+            0x01, 0x01, 0x01, 0x01 // Reserved
+    };
+    const uint8_t _message_GSA[200] = {
+            0xF0, 0x02,            // NMEA ID for GSA
+            0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+            0x01,                  // Enable
+            0x01, 0x01, 0x01, 0x01 // Reserved
+    };
+    const uint8_t _message_GSV[200] = {
+        0xF0, 0x03,            // NMEA ID for GSV
+                0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+                0x00,                  // Disable
+                0x01, 0x01, 0x01, 0x01 // Reserved
+    };
+    const uint8_t _message_VTG[200] = {
+            0xF0, 0x05,            // NMEA ID for VTG
+            0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+            0x00,                  // Disable
+            0x01, 0x01, 0x01, 0x01 // Reserved
+    };
+    const uint8_t _message_RMC[200] = {
+            0xF0, 0x04,            // NMEA ID for RMC
+            0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+            0x01,                  // Enable
+            0x01, 0x01, 0x01, 0x01 // Reserved
+    };
+    const uint8_t _message_GGA[200] = {
+            0xF0, 0x00,            // NMEA ID for GGA
+            0x01,                  // I/O Target 0=I/O, 1=UART1, 2=UART2, 3=USB, 4=SPI
+            0x01,                  // Enable
+            0x01, 0x01, 0x01, 0x01 // Reserved
+    };
 
 };
 
