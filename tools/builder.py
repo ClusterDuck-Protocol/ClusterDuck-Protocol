d81e9ef198a28e0fc38cfbcba9eace7b8ef191ff
Amir Nathoo <amirna2@users.noreply.github.com>
2024-06-28 15:54:06 -0700

    Platform IO updates - improved builder cli - updated platform INI to use symlink
    Signed-off-by: Amir Nathoo <amirna2@users.noreply.github.com>

--- tools/builder.py
+++ tools/builder.py
@@ -5,6 +5,10 @@ import subprocess
 import re
 from pathlib import Path
 
+prog = 'builder'
+
+version = '1.0.0'
+
 def get_platformio_envs():
     """Retrieve list of environments from platformio.ini or through platformio CLI."""
     ini_path = Path('platformio.ini')
@@ -38,38 +42,50 @@ def run_command(command, target, deploy = False, suite = None, clean = False):
             cmd += ['-t', 'upload']
         if suite and command == 'test':
             cmd += ['-f', suite]
-        if clean and command == 'run':
-            cmd += ['-t', 'clean']
 
-    print(f"\033[1;93m cdp_cli: executing {' '.join(cmd)}\033[0m")
-    subprocess.run(cmd)
+
+    print(f"\033[1;93m warp builder: executing {' '.join(cmd)}\033[0m")
+    # remove .pio folder before building
+    if clean:
+        subprocess.run(['rm', '-rf', '.pio'])
+
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        print(f"Error: {' '.join(cmd)} failed.")
+        exit(1)
 
 def main():
-    parser = argparse.ArgumentParser(description='CDP - A PlatformIO CLI Wrapper to build CDP')
+    parser = argparse.ArgumentParser(description='builder - A PlatformIO CLI Wrapper to build warp library and examples.')
     subparsers = parser.add_subparsers(dest='command', help=f'Available commands')
-    
+
+    # Version command
+    version_parser = subparsers.add_parser('version', help='Print cli version')
+
     # Build/test command
-    for cmd in ['build', 'test', 'clean']:
+    for cmd in ['build', 'test']:
+
         cmd_parser = subparsers.add_parser(cmd, help=f'{cmd} the project for a specific target')
         cmd_parser.add_argument('-t', '--target', required=True, help='Target environment to build/test/clean')
         if cmd == 'build':
             cmd_parser.add_argument('--deploy', action='store_true', help='Deploy after build')
+            cmd_parser.add_argument('--clean', action='store_true', help='Clean before build')
         elif cmd == 'test':
             cmd_parser.add_argument('-s', '--suite', help='Test suite to run')
-        
+
     # List targets command
     list_parser = subparsers.add_parser('list-targets', help='list all available target environments')
 
     args = parser.parse_args()
-    
-    if args.command in ['build', 'test', 'clean']:
+
+    if args.command in ['build', 'test']:
         # Validate target
         envs = get_platformio_envs()
         if args.target not in envs:
             print(f"Error: Target '{args.target}' is not a valid environment. Available environments are: {', '.join(envs)}")
             exit(1)
-        
-        # Map command from cdp to platformio
+
+        # Map command from warp to platformio
 
         # pio_command = 'run' if args.command == 'build' or 'clean' else 'test'
         if args.command == 'build' or args.command == 'clean':
@@ -79,13 +95,15 @@ def main():
         else:
             pio_command = args.command
 
-        run_command(pio_command, args.target, 
-                    getattr(args, 'deploy', False), 
+        run_command(pio_command, args.target,
+                    getattr(args, 'deploy', False),
                     getattr(args, 'suite', None),
-                    getattr(args, 'clean', (args.command == 'clean')))
+                    getattr(args, 'clean', False))
 
     elif args.command == 'list-targets':
         list_targets()
+    elif args.command == 'version':
+        print(f'builder cli: v{version}')
     else:
         parser.print_help()
 
