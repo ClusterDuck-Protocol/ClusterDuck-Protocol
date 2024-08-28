6f08da4cbbbc9118a388cbf8da240bb0bdaa9bdd
Charlie Evans <charlie.glenn.evans.jr@gmail.com>
2024-08-28 08:41:45 -0500

    Fixing example folder numbers
    Signed-off-by: Charlie Evans <charlie.glenn.evans.jr@gmail.com>

---
+++ examples/5.Custom-Board/DuckLink/cdp_external_board.h
@@ -0,0 +1,13 @@
+#ifndef CDP_EXTERNAL_BOARD_H
+#define CDP_EXTERNAL_BOARD_H
+
+
+#ifdef HELTEC_WIRELESS_PAPER
+#include "my-boards/heltec_wireless_paper/board_config.h"
+#endif
+
+#ifdef HELTEC_CUBE_CELL_GPS
+#include "my-boards/heltec_cubecell_gps/board_config.h"
+#endif
+
+#endif // CDP_EXTERNAL_BOARD_H
