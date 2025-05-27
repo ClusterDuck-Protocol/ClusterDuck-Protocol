5f4fcff282a203386f1f265eba52627283178e27
Taraqur Rahman <tqrahman8@gmail.com>
2025-05-27 08:30:36 -0400

    Ducklink (#484)
    * updated DuckLink to reflect Mama
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * added doc to DuckLink
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * changed delimiter to |
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * used | as delimiter
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * added topics as a parameter
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * started documentation
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * removed stringToByteVector
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * added docs and topics
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * simplified print statement
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    * removed vector from examples
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>
    
    ---------
    
    Signed-off-by: Taraqur <tqrahman8@gmail.com>

--- examples/Basic-Ducks/DuckLink/DuckLink.ino
+++ examples/Basic-Ducks/DuckLink/DuckLink.ino
@@ -1,63 +1,60 @@
-
 /**
  * @file DuckLink.ino
- * @author
- * @brief This example creates a duck link that sends a counter message periodically.
- *
- * It's using a pre-built DuckLink available from the ClusterDuck SDK
+ * @brief Implements a DuckLink using the ClusterDuck Protocol (CDP).
+ * 
+ * This example firmware periodically sends sensor health data (counter and free memory) 
+ * through a CDP mesh network.
+ * 
+ * @date 2025-05-07
  */
 
 #include <arduino-timer.h>
 #include <string>
-#include <vector>
 #include <CDP.h>
-
 
 #ifdef SERIAL_PORT_USBVIRTUAL
 #define Serial SERIAL_PORT_USBVIRTUAL
 #endif
 
-// create a built-in duck link
-DuckLink duck;
-bool sendSensorData();
+// --- Function Declarations ---
 bool runSensor(void *);
-// create a timer with default settings
-auto timer = timer_create_default();
+bool sendData(std::string message, byte topic=topics::status);
 
-// for sending the counter message
-const int INTERVAL_MS = 30000;
-int counter = 1;
+// --- Global Variables ---
+DuckLink duck;                            // CDP DuckLink instance
+auto timer = timer_create_default();      // Creating a timer with default settings
+const int INTERVAL_MS = 10000;            // Interval in milliseconds between each call of runSensor
+int counter = 1;                          // Message counter to track transmissions
+bool setupOK = false;                     // Flag indicating setup completion status
 
-bool setupOK = false;
-
+/**
+ * @brief Setup function initializing the DuckLink.
+ *
+ * - Sets up the Duck ID (exactly 8 bytes).
+ * - Initializes DuckLink using default configuration.
+ * - Sets up periodic execution of sensor data transmissions.
+ */
 void setup() {
-  // We are using a hardcoded device id here, but it should be retrieved or
-  // given during the device provisioning then converted to a byte vector to
-  // setup the duck NOTE: The Device ID must be exactly 8 bytes otherwise it
-  // will get rejected
-  std::string deviceId("DUCK0001");
+
+  std::string deviceId("DUCK0001");    // MUST be 8 bytes and unique from other ducks
   std::array<byte,8> devId;
-  int rc;
   std::copy(deviceId.begin(), deviceId.end(), devId.begin());
-  rc = duck.setupWithDefaults(devId);
-  if (rc != DUCK_ERR_NONE) {
-    Serial.print("[LINK] Failed to setup ducklink: rc = ");
-    Serial.println(rc);
+  if (duck.setupWithDefaults(devId) != DUCK_ERR_NONE) {
+    Serial.println("[LINK] Failed to setup DuckLink");
     return;
   }
-
-  setupOK = true;
-  // Initialize the timer. The timer thread runs separately from the main loop
-  // and will trigger sending a counter message.
+  
+  timer.every(INTERVAL_MS, runSensor);     // Triggers runSensor every INTERVAL_MS
+  
   Serial.println("[LINK] Setup OK!");
-
-  if (sendSensorData()) {
-    timer.every(INTERVAL_MS, runSensor);
-  } else {
-    Serial.println("[LINK] ERROR - Failed to send sensor data");
-  }
+  setupOK = true;
 }
 
+/**
+ * @brief Main loop runs continuously.
+ *
+ * Executes scheduled tasks and maintains Duck operation.
+ */
 void loop() {
   if (!setupOK) {
     return; 
@@ -67,37 +64,50 @@ void loop() {
   duck.run();
 }
 
-std::vector<byte> stringToByteVector(const std::string& str) {
-    std::vector<byte> byteVec;
-    byteVec.reserve(str.length());
-
-    for (unsigned int i = 0; i < str.length(); ++i) {
-        byteVec.push_back(static_cast<byte>(str[i]));
-    }
-
-    return byteVec;
-}
-
-bool sendSensorData() {
+/**
+ * @brief Function to run the sensor and send data.
+ *
+ * This function is called periodically to send sensor health data.
+ *
+ * @param unused Pointer to unused parameter (not used in this implementation)
+ * @return true if data was sent successfully, false otherwise
+ */
+bool runSensor(void *) {
+  
   bool result = false;
-  const byte* buffer;
 
-  std::string message = std::string("Counter:") + std::to_string(counter);
+  std::string message = "C:" + std::to_string(counter) + "|" + "FM:" + std::to_string(freeMemory());
   Serial.print("[LINK] sensor data: ");
   Serial.println(message.c_str());
-  std::vector<byte> data = stringToByteVector(message);
-  int err = duck.sendData(topics::status, data.data(),data.size());
-  if (err == DUCK_ERR_NONE) {
-     result = true;
-     counter++;
+  
+  result = sendData(message, topics::health);
+  if (result) {
+     Serial.println("[LINK] runSensor ok.");
   } else {
-    std::string errMessage = "[LINK] Failed to send data. error = " + std::to_string(err);
-    Serial.println(errMessage.c_str());
-    return false;
+     Serial.println("[LINK] runSensor failed.");
   }
   return result;
+
 }
 
-bool runSensor(void *) {
-  return sendSensorData();
+/**
+ * @brief Sends the provided message as CDP packet to the mesh network.
+ *
+ * Encapsulates the message within a CDP topic and handles errors in transmission. 
+ *
+ * @param message The payload data to send as a std::string
+ * @param topic CDP topic. CDP topics can be found in CdpPacket.h (default: status)
+ * @return true if data sent successfully, false otherwise
+ */
+bool sendData(std::string message, byte topic) {
+  bool sentOk = false;
+  
+  int err = duck.sendData(topic, message);
+  if (err == DUCK_ERR_NONE) {
+    sentOk = true;
+  }
+  if (!sentOk) {
+    Serial.println(("[Link] Failed to send data. error = " + std::to_string(err)).c_str());
+  }
+  return sentOk;
 }
--- examples/Basic-Ducks/MamaDuck/MamaDuck.ino
+++ examples/Basic-Ducks/MamaDuck/MamaDuck.ino
@@ -1,10 +1,15 @@
 /**
  * @file MamaDuck.ino
- * @brief Uses the built in Mama Duck.
+ * @brief Implements a MamaDuck using the ClusterDuck Protocol (CDP).
+ * 
+ * This example firmware periodically sends sensor health data (counter and free memory) 
+ * through a CDP mesh network. It also relays messages that it receives from other ducks
+ * that has not seen yet.
+ * 
+ * @date 2025-05-07
  */
 
 #include <string>
-#include <vector>
 #include <arduino-timer.h>
 #include <CDP.h>
 
@@ -12,26 +17,27 @@
 #define Serial SERIAL_PORT_USBVIRTUAL
 #endif
 
-bool sendData(std::vector<byte> message);
+// --- Function Declarations ---
+bool sendData(std::string message, byte topic=topics::status);
 bool runSensor(void *);
 
-// create a built-in mama duck
-MamaDuck duck;
+// --- Global Variables ---
+MamaDuck duck;                        // CDP MamaDuck instance
+auto timer = timer_create_default();  // Creating a timer with default settings
+const int INTERVAL_MS = 10000;        // Interval in milliseconds between runSensor call
+int counter = 1;                      // Counter for the sensor data  
+bool setupOK = false;                 // Flag to check if setup is complete
 
-// create a timer with default settings
-auto timer = timer_create_default();
-
-// for sending the counter message
-const int INTERVAL_MS = 60000;
-int counter = 1;
-bool setupOK = false;
-
+/**
+ * @brief Setup function to initialize the MamaDuck
+ *
+ * - Sets up the Duck device ID (exactly 8 bytes).
+ * - Initializes MamaDuck using default configuration.
+ * - Sets up periodic execution of sensor data transmissions.
+ */
 void setup() {
-  // We are using a hardcoded device id here, but it should be retrieved or
-  // given during the device provisioning then converted to a byte vector to
-  // setup the duck NOTE: The Device ID must be exactly 8 bytes otherwise it
-  // will get rejected
-  std::string deviceId("MAMA0001");
+
+  std::string deviceId("MAMA0001"); // MUST be 8 bytes and unique from other ducks
   std::array<byte,8> devId;
   std::copy(deviceId.begin(), deviceId.end(), devId.begin());
   if (duck.setupWithDefaults(devId) != DUCK_ERR_NONE) {
@@ -39,64 +45,70 @@ void setup() {
     return;
   }
 
+  timer.every(INTERVAL_MS, runSensor); // Triggers runSensor every INTERVAL_MS
+  
   setupOK = true;
-
-  // Initialize the timer. The timer thread runs separately from the main loop
-  // and will trigger sending a counter message.
-  timer.every(INTERVAL_MS, runSensor);
   Serial.println("[MAMA] Setup OK!");
-
 }
 
-std::vector<byte> stringToByteVector(const std::string& str) {
-    std::vector<byte> byteVec;
-    byteVec.reserve(str.length());
-
-    for (unsigned int i = 0; i < str.length(); ++i) {
-        byteVec.push_back(static_cast<byte>(str[i]));
-    }
-
-    return byteVec;
-}
-
+/**
+ * @brief Main loop runs continuously.
+ *
+ * Executes scheduled tasks and maintains Duck operation.
+ */
 void loop() {
   if (!setupOK) {
     return; 
   }
   timer.tick();
-  // Use the default run(). The Mama duck is designed to also forward data it receives
-  // from other ducks, across the network. It has a basic routing mechanism built-in
-  // to prevent messages from hoping endlessly.
+
   duck.run();
 }
 
+/**
+ * @brief Periodically executed to gather and send health data.
+ *
+ * Collects the current counter value and available free memory, formats them
+ * into a string, and transmits this data via CDP.
+ *
+ * @param unused Unused parameter required by the timer callback signature
+ * @return true if data was successfully sent, false otherwise
+ */
 bool runSensor(void *) {
   bool result;
   
-  std::string message = std::string("Counter:") + std::to_string(counter) + " " + std::string("Free Memory:") + std::to_string(freeMemory());
+  std::string message = "C:" + std::to_string(counter) + "|" + "FM:" + std::to_string(freeMemory());
   Serial.print("[MAMA] sensor data: ");
   Serial.println(message.c_str());
 
-  result = sendData(stringToByteVector(message));
+  result = sendData(message, topics::health);
   if (result) {
-     Serial.println("[MAMA] runSensor ok.");
+    counter++;
+    Serial.println("[MAMA] runSensor ok.");
   } else {
-     Serial.println("[MAMA] runSensor failed.");
+    Serial.println("[MAMA] runSensor failed.");
   }
   return result;
 }
 
-bool sendData(std::vector<byte> message) {
+/**
+ * @brief Sends the provided message as CDP packet to the mesh network.
+ *
+ * Encapsulates the message within a CDP topic and handles errors in transmission. 
+ *
+ * @param message The payload data to send as a std::string
+ * @param topic CDP topic. CDP topics can be found in CdpPacket.h (default: status)
+ * @return true if data sent successfully, false otherwise
+ */
+bool sendData(std::string message, byte topic) {
   bool sentOk = false;
   
-  int err = duck.sendData(topics::status, message);
+  int err = duck.sendData(topic, message);
   if (err == DUCK_ERR_NONE) {
-     counter++;
-     sentOk = true;
+    sentOk = true;
   }
   if (!sentOk) {
-    std::string errMessage = "[MAMA] Failed to send data. error = " + std::to_string(err);
-    Serial.println(errMessage.c_str());
+    Serial.println(("[Link] Failed to send data. error = " + std::to_string(err)).c_str());
   }
   return sentOk;
 }
