5c025c826eae3b2470d8c85f64cb15747c950717
faraday <rcheyenne.truss@gmail.com>
2025-06-17 13:11:57 -0500

    Release 4.4.0 (#489)
    * update version number
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    * remove custom-mama-detect from build
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    * Debug printout changes (#487)
    
    * making printouts more readable
    
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>
    
    * std::array fix
    
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>
    
    * fix out of bounds error
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    ---------
    
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    Co-authored-by: faradaym <rcheyenne.truss@gmail.com>
    
    * fix crash on log and show logs as text
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    * fix hops and type
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    * fix crc
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    
    * update versions
    
    ---------
    
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>
    Co-authored-by: Brenton Poke <brentonpoke@outlook.com>

--- src/DuckPacket.cpp
+++ src/DuckPacket.cpp
@@ -36,13 +36,13 @@ bool DuckPacket::prepareForRelaying(BloomFilter *filter, std::vector<byte> dataB
   
 }
 
-void DuckPacket::getUniqueMessageId(BloomFilter * filter, byte message_id[MUID_LENGTH]) {
+void DuckPacket::getUniqueMessageId(BloomFilter * filter, std::array<uint8_t,MUID_LENGTH> &message_id) {
 
   bool getNewUnique = true;
   while (getNewUnique) {
-    duckutils::getRandomBytes(MUID_LENGTH, message_id);
-    getNewUnique = filter->bloom_check(message_id, MUID_LENGTH);
-    loginfo_ln("prepareForSending: new MUID -> %s",duckutils::convertToHex(message_id, MUID_LENGTH).c_str());
+    duckutils::getRandomBytes(MUID_LENGTH, message_id.data());
+    getNewUnique = filter->bloom_check(message_id.data(), MUID_LENGTH);
+    loginfo_ln("prepareForSending: new MUID -> %s",duckutils::convertToHex(message_id.data(), MUID_LENGTH).c_str());
   }
 }
 
@@ -61,10 +61,10 @@ int DuckPacket::prepareForSending(BloomFilter *filter,
 
   loginfo_ln("prepareForSending: DATA LENGTH: %d - TOPIC (%s)", app_data_length, CdpPacket::topicToString(topic).c_str());
 
-  byte message_id[MUID_LENGTH];
+  std::array<uint8_t,MUID_LENGTH> message_id;
   getUniqueMessageId(filter, message_id);
 
-  byte crc_bytes[DATA_CRC_LENGTH];
+  std::array<uint8_t,DATA_CRC_LENGTH> crc_bytes;
   uint32_t value;
   // TODO: update the CRC32 library to return crc as a byte array
   if(duckcrypto::getState()) {
@@ -80,33 +80,45 @@ int DuckPacket::prepareForSending(BloomFilter *filter,
   crc_bytes[3] = value & 0xFF;
 
   // ----- insert packet header  -----
+    loginfo_ln("prepareForSending: START");
+    std::string printStr;
   // source device uid
     buffer.insert(buffer.end(), duid.begin(), duid.end());
-    logdbg_ln("SDuid:     %s",duckutils::convertToHex(duid.data(), duid.size()).c_str());
 
-    // destination device uid
-    buffer.insert(buffer.end(), targetDevice.begin(), targetDevice.end());
-    logdbg_ln("DDuid:     %s", duckutils::convertToHex(targetDevice.data(), targetDevice.size()).c_str());
+    std::string strDuid(duid.begin(), duid.end());
+    logdbg_ln("SDuid:      %s", strDuid.c_str());
 
+
+  // destination device uid  
+    if (targetDevice == BROADCAST_DUID){
+      logdbg_ln("DDuid:     broadcast");
+    } else{
+      buffer.insert(buffer.end(), targetDevice.begin(), targetDevice.end());
+      logdbg_ln("DDuid: %s", std::string(targetDevice.begin(), targetDevice.end()).c_str());
+    }
+    
     // message uid
-    buffer.insert(buffer.end(), &message_id[0], &message_id[MUID_LENGTH]);
-    logdbg_ln("Muid:      %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+    buffer.insert(buffer.end(), message_id.begin(), message_id.end());
+    printStr.assign(message_id.begin(), message_id.end());
+    logdbg_ln("Muid:      %s", printStr.c_str());
 
     // topic
     buffer.insert(buffer.end(), topic);
-    logdbg_ln("Topic:     %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+    logdbg_ln("Topic:     %s", CdpPacket::topicToString(topic).c_str());
 
     // duckType
     buffer.insert(buffer.end(), duckType);
-    logdbg_ln("duck type: %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+    byte deviceType = buffer[DUCK_TYPE_POS];
+    logdbg_ln("duck type: %s", std::to_string(deviceType).c_str());
 
     // hop count
     buffer.insert(buffer.end(), 0x00);
-    logdbg_ln("hop count: %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+    byte hopCount = buffer[HOP_COUNT_POS];
+    logdbg_ln("hop count: %s", std::to_string(hopCount).c_str());
 
     // data crc
-    buffer.insert(buffer.end(), &crc_bytes[0], &crc_bytes[DATA_CRC_LENGTH]);
-    logdbg_ln("Data CRC:  %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+    buffer.insert(buffer.end(), crc_bytes.begin(), crc_bytes.end());
+    logdbg_ln("Data CRC:  %s", duckutils::convertToHex(crc_bytes.data(), DATA_CRC_LENGTH - 1).c_str());
 
     // ----- insert data -----
     if(duckcrypto::getState()) {
@@ -116,11 +128,12 @@ int DuckPacket::prepareForSending(BloomFilter *filter,
 
     } else {
         buffer.insert(buffer.end(), app_data.begin(), app_data.end());
-        logdbg_ln("Data:      %s",duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+        //convert the data to string for logging
+        logdbg_ln("Data:      %s",printStr.assign(app_data.begin(), app_data.end()).c_str());
     }
-
-    logdbg_ln("Built packet: %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
     getBuffer().shrink_to_fit();
+    logdbg_ln("Built packet (HEX): %s", duckutils::convertToHex(buffer.data(), buffer.size()).c_str());
+
   return DUCK_ERR_NONE;
 }
 
