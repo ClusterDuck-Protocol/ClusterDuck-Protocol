4765085820fd327835171d0592800638f61cd90d
faradaym <rcheyenne.truss@gmail.com>
2025-10-06 13:38:01 -0500

    move wifi related functions from duckutils to wifi
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>

--- src/utils/DuckUtils.cpp
+++ src/utils/DuckUtils.cpp
@@ -72,80 +72,6 @@ uint32_t toUint32(const uint8_t* data) {
     return value;
 }
 
-#ifdef CDPCFG_WIFI_NONE
-int saveWifiCredentials(std::string ssid, std::string password) {
-  logwarn_ln("WARNING saveWifiCredentials skipped, device has no WiFi.");
-  return DUCK_ERR_NOT_SUPPORTED;
-}
-
-std::string loadWiFiPassword() {
-  logwarn_ln("WARNING loadWiFiPassword skipped, device has no WiFi.");
-  return "unknown";
-}
-std::string loadWifiSsid() {
-  logwarn_ln("WARNING loadWifiSsid skipped, device has no WiFi.");
-  return "unknown";
-}
-
-#else
-int saveWifiCredentials(std::string ssid, std::string password) {
-  int err = DUCK_ERR_NONE;
-
-  if (ssid.empty() || password.empty()) {
-    logerr("Invalid SSID or password\n");
-    return DUCK_ERR_INVALID_ARGUMENT;
-  }
-  if (!EEPROM.begin(512)) {
-    logerr("Failed to initialise EEPROM\n");
-    return DUCK_ERR_EEPROM_INIT;
-  }
-
-  if (ssid.length() > 0 && password.length() > 0) {
-    loginfo("Clearing EEPROM\n");
-    for (int i = 0; i < 96; i++) {
-      EEPROM.write(i, 0);
-    }
-
-    loginfo("updating EEPROM...\n");
-    for (int i = 0; i < ssid.length(); i++)
-    {
-      EEPROM.write(i, ssid[i]);
-    }
-    for (int i = 0; i < password.length(); ++i)
-    {
-      EEPROM.write(32 + i, password[i]);
-    }
-    if (!EEPROM.commit()) {
-      logerr("Failed to commit EEPROM\n");
-      err = DUCK_ERR_EEPROM_WRITE;
-    }
-  }
-  return err;
-}
-
-std::string loadWifiSsid() {
-  EEPROM.begin(512); //Initialasing EEPROM
-  std::string esid;
-  // loop through saved SSID characters
-  for (int i = 0; i < 32; ++i)
-  {
-    esid += char(EEPROM.read(i));
-  }
-  return esid;
-}
-
-std::string loadWifiPassword() {
-  EEPROM.begin(512); //Initialasing EEPROM
-  std::string epass = "";
-  // loop through saved Password characters
-  for (int i = 32; i < 96; ++i)
-  {
-    epass += char(EEPROM.read(i));
-  }
-  return epass;
-}
-#endif
-
 std::string toUpperCase(std::string str) {
   std::string upper = "";
   for (int i = 0; i < str.length(); i++) {
@@ -228,4 +154,4 @@ std::string toUpperCase(std::string str) {
     return "Unknown error";
   }
 
-} // namespace duckutils
+}
