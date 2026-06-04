be0cb41f7c8db7e781d331233a1cdc02b0385c4a
faradaym <rcheyenne.truss@gmail.com>
2026-06-04 15:06:40 -0500

    testing interval signalinfo
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>

--- src/Ducks/Duck.h
+++ src/Ducks/Duck.h
@@ -486,7 +486,7 @@ class Duck {
 
     //Telemetry
     const int HEALTH_INTERVAL = 1000 * 60 * 15; //15 minutes
-    const int SIGNAL_INTERVAL = 1000 * 5;//0 * 7; //1 hour 2 min
+    const int SIGNAL_INTERVAL = 1000 * 60;//0 * 7; //1 hour 2 min
     int counter = 1;
     Timer<10> duckTimer;
 
