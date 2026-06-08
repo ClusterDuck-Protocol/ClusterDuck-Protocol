1e556e1f88448f6fc76d126d0c99433fd5f88ad0
TimoWielink <timowielink@gmail.com>
2026-06-08 12:21:53 -0400

    10 omiute timer
    Signed-off-by: TimoWielink <timowielink@gmail.com>

--- src/Ducks/Duck.h
+++ src/Ducks/Duck.h
@@ -486,7 +486,7 @@ class Duck {
 
     //Telemetry
     const int HEALTH_INTERVAL = 1000 * 60 * 15; //15 minutes
-    const int SIGNAL_INTERVAL = 1000 * 60 * 4; //4 minutes
+    const int SIGNAL_INTERVAL = 1000 * 60 * 10; //10 minutes
     int counter = 1;
     Timer<10> duckTimer;
 
